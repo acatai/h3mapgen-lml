0c0448cb0d7fdf41af261e5bc6add8ac5debe48f
acatai <cutterthecat@gmail.com>
2017-02-09 23:23:41 +0100

    Lua/C api test - config reading

---
+++ _lua-c_api_test/compile.sh
@@ -0,0 +1,6 @@
+
+# http://www.mateuszmidor.com/2011/05/debugowanie-bledow-pamieci-pod-linuxem-valgrind-memcheck/
+
+# g++ testmain.cpp  -o test   -I/usr/local/include/luajit-2.0  -lluajit-5.1 -fPIC
+
+
